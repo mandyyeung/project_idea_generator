5bca9231ccb54d9e9338df4d5d8ff0824eb79226
Mandy Yeung <mandycodes@gmail.com>
2014-09-03 14:17:06 -0400

    Started jquery randomizer

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+var idea = ['red', 'black', 'blue', 'green', 'yellow'];
 
+$('#give').click(function(){
+  var index = Math.round(Math.random()*(idea.length - 1));
+  $('input').val(idea[index]);
+  console.log(index);
+})
