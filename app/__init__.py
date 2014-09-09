443503ae0fd75f6406379e37bc3b6186c44b9193
Mandy Yeung <mandycodes@gmail.com>
2014-09-09 16:03:55 -0400

    Add a login button and route

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session
+from flask import Flask, render_template, request, session, redirect, url_for
 app = Flask(__name__)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
@@ -21,6 +21,14 @@ def form():
 	else:
 		session['name'] = request.form['name']
 		return "Your name is " + request.form['name'] + " and it has been stored in the session"
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+  if request.method == "GET":
+    return render_template('login.html')
+  else:
+    session['name'] = request.form['name']
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000)
