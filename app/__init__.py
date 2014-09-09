5d63d2deaf2075680c2c80d48785d0d3c58580c7
Jon Chan <jchan@stackoverflow.com>
2014-09-09 15:39:20 -0400

    Sessions, http tutorial files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route("/")
 def index():
     return render_template('index.html')
+
+@app.route("/users/<id>")
+def users(id):
+	users = [
+		{"name": "Jon"},
+		{"name": "Mandy"}
+	]
+	return str(users[int(id)])
+
+@app.route("/form", methods = ['GET', 'POST'])
+def form():
+	if request.method == "GET":
+		return render_template('form.html', name = session.get('name', ""))
+	else:
+		session['name'] = request.form['name']
+		return "Your name is " + request.form['name'] + " and it has been stored in the session"
 
 if __name__ == "__main__":
     app.run(debug = True, port = 5000)
